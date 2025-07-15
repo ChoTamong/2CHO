94a39afafc52d0a0733bece8157ae58739a7191b
SeYunJung <77439732+SeYunJung@users.noreply.github.com>
2025-07-15 09:48:44 +0900

    [Update] 프로젝트 생성

---
+++ TIME EATER/TIME EATER/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace TIME_EATER
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
